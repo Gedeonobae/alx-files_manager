4f1872d679837fe61b1ba3c4866acdd27b78bc1b
Gedeon Obae Gekonge <85337093+Gedeonobae@users.noreply.github.com>
2022-08-25 01:06:36 +0300

    create the Express server

---
+++ routes/index.js
@@ -0,0 +1,20 @@
+import express from 'express';
+import AppController from '../controllers/AppController';
+import UsersController from '../controllers/UsersController';
+
+export default function Routes(app) {
+  const router = express.Router();
+  app.use('/', router);
+
+  router.get('/status', (req, res) => {
+    AppController.getStatus(req, res);
+  });
+
+  router.get('/stats', (req, res) => {
+    AppController.getStats(req, res);
+  });
+
+  router.post('/users', (req, res) => {
+    UsersController.postNew(req, res);
+  });
+}
